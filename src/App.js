1539b31e0fc0211dcd1b3c9d26640dc65eab47b6
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-05-05 20:19:05 +0300

    fix Subtask3

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      path: '/'
+      path: '/',
+      value: ' '
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
+    this.setState({ value: event.target.value })
     if (event.target.value % 2 !== 0) {
       this.setState({ path: '/subtask3' })
     }
@@ -26,8 +28,7 @@ class App extends React.Component {
         <h2>The topic 'Routes'</h2>
         <Link to={path}>Show protected information if</Link>
         <span>&nbsp;</span>
-        {/* <input size="5" onChange={this.handleChange} type="text" name="input" ></input> is odd */}
-        <input size="5"></input> is odd
+        <input size="5" onChange={this.handleChange} type="text" name="input" value={this.state.value}></input> is odd 
         <div className="mainClass">
           Go to the component programmatically, by checking the box:{" "}
           <input type="checkbox"></input>
