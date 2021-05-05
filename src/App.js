dc48d1b769e78ac838e96b5d8b7b39f605300a8e
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-05-05 20:01:59 +0300

    fix Subtask3

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
         <h2>The topic 'Routes'</h2>
         <Link to={path}>Show protected information if</Link>
         <span>&nbsp;</span>
-        <input size="5" onChange={this.handleChange} ></input> is odd
+        <input size="5" onChange={this.handleChange} type="text" name="input" ></input> is odd
         <div className="mainClass">
           Go to the component programmatically, by checking the box:{" "}
           <input type="checkbox"></input>
