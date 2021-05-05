b3e5f887b8718f487aa5c168c935fe646acefda4
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-05-05 17:49:55 +0300

    change render Subtask2

--- src/components/Subtask2.js
+++ src/components/Subtask2.js
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 
 export default function subtask2({ location, match }) {
     const optionalParameters = queryString.parse(location.search);
-    const paramItems = Object.keys(optionalParameters).map((key, index, array) => `${array[index]} = ${optionalParameters[key]} `);
+    const paramItems = Object.keys(optionalParameters).map((key, index, array) =>`${array[index]}=${optionalParameters[key]} `);
   return (
     <div>
           Subtask2, query parameters: {paramItems}
