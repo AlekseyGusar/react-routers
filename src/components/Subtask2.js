887b2af0be920da96658c8c430be4eba9d9ccc4b
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-05-05 17:58:15 +0300

    add coma

--- src/components/Subtask2.js
+++ src/components/Subtask2.js
@@ -3,7 +3,13 @@ import queryString from 'query-string';
 
 export default function subtask2({ location, match }) {
     const optionalParameters = queryString.parse(location.search);
-    const paramItems = Object.keys(optionalParameters).map((key, index, array) =>`${array[index]}=${optionalParameters[key]} `);
+    const paramItems = Object.keys(optionalParameters).map((key, index, array) => {
+        let coma = ',';
+        if (index===array.length -1){
+            coma = '';
+        };
+        return `${array[index]}=${optionalParameters[key]}${coma} `
+    });
   return (
     <div>
           Subtask2, query parameters: {paramItems}
